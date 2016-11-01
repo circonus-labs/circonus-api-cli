a5f66a11ed7617abdc2eaded67179815ef6e2a64
misale <alem.abreha@gmail.com>
2016-11-01 10:34:33 -0400

    second commit

--- circonusapi/cnsfilter.go
+++ circonusapi/cnsfilter.go
@@ -1,3 +1,6 @@
+// Copyright 2016 Alem Abreha. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
 package circonusapi
 
 type AnnotationFilter struct {
